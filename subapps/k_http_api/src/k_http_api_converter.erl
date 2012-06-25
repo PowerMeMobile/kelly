8a6c64353446078357eabfd356558db2fb3f9537
Anton Sizov <a.sizov@dev1team.net>
2012-06-25 11:57:22 +0300

    Fix json representation

--- subapps/k_http_api/src/k_http_api_converter.erl
+++ subapps/k_http_api/src/k_http_api_converter.erl
@@ -2,7 +2,7 @@
 
 -define(xml_prolog, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>").
 
--export([process/2, test/0]).
+-export([process/2]).
 
 -include_lib("k_common/include/logging.hrl").
 
@@ -25,114 +25,29 @@ process(Response, _AnySuff) ->
 
 convert_to_xml(Response) ->
 	?log_debug("Response: ~p", [Response]),
-	Converted = preprocess(Response),
+	Converted = xml_preprocess(Response),
 	?log_debug("Converted: ~p", [Converted]),
 	Xml = xmerl:export_simple(Converted, xmerl_xml,[{prolog, ?xml_prolog}]),
-	% ?log_debug("Xml: ~p", [Xml]),
+	?log_debug("Xml: ~p", [Xml]),
 	BinXml = unicode:characters_to_binary(Xml),
 	{ok, BinXml}.
 
-%% %% make possible to process List of PropLists
-%% xml_preprocess(Response) when is_list(Response) ->
-%% 	lists:map(fun(PropList)->
-%% 		xml_preprocess(PropList, [])
-%% 	end, Response);
-%% xml_preprocess(Response) ->
-%% 	xml_preprocess([Response]).
-
-
-%% %% converting proplist to xmerl compatible format
-%% 	%% in case of Value = []
-%% xml_preprocess({Name, []}, []) ->
-%% 	{Name, ["undefined"]};
-%% 	%%
-%% xml_preprocess({Name, []}, Acc) ->
-%% 	{Name, lists:reverse(Acc)};
-%% 	%% make possible to chek list in entire depth
-%% xml_preprocess({Name, [{SubName, SubValue} | Tail]}, Acc) ->
-%% 	Sub = xml_preprocess({SubName, SubValue}, []),
-%% 	%%  is not tail recursion
-%% 	xml_preprocess({Name, Tail}, [Sub | Acc]);
-%% 	%% only atom converts
-%% xml_preprocess({Name, [Value | Tail]}, []) when is_atom(Value) ->
-%% 	AtomString = convert_atom_list([Value | Tail]),
-%% 	{Name, AtomString};
-%%  	%% for such values as ["string1", "string2"]
-%% xml_preprocess({Name, String = [Value | Tail]}, []) when is_list(Value) ->
-%% 	StringList = string:join(String, ","),
-%% 	{Name, [StringList]};
-%% 	%% for such values as {atom, "string"}
-%% xml_preprocess({Name, Value}, []) when is_list(Value) ->
-%% 	{Name, [Value]};
-%% 	%% for other values such as integer
-%% xml_preprocess({Name, Value}, Acc) ->
-%% 	xml_preprocess({Name, []}, [io_lib:format("~p", [Value]) | Acc]).
-
-%% xml_preprocess(List = [{K, V} | Rest]) ->
-%% 	{item, List}
-%% xml_preprocess(List = [Item | Rest]) when is_list(Item) ->
-
-
-%% xml_preprocess({K, V})  ->
-%% 	[{K, xml_preprocess(V)}].
-
-preprocess([]) ->
-	[];
-%preprocess{[List | Rest]}
-preprocess({K, V}) ->
-	[{K,preprocess(V)}].
-
-
-%% json_preprocess(null) ->
-%% 	null;
-%% json_preprocess(undefined) ->
-%% 	null;
-%% json_preprocess(true) ->
-%% 	true;
-%% json_preprocess(false) ->
-%% 	false;
-%% json_preprocess(Binary) when is_binary(Binary) ->
-%% 	Binary;
-%% json_preprocess(Number) when is_integer(Number) orelse
-%% 							  is_float(Number) ->
-%% 	Number;
-%% json_preprocess(Atom) when is_atom(Atom) ->
-%% 	atom_to_binary(Atom, utf8);
-
-%% preprocess(List = [{_K, _V} | _Rest]) ->
-%% 	[{Key, preprocess(Value)} || {Key, Value} <- List];
-%% 	%% for such values as ["string1", "string2"]
-%% preprocess(List = [E | _Rest]) when is_list(E) ->
-%% 	?log_debug("List: ~p", [List]),
-%% 	[Element || Element <- List];
-%% preprocess(List = [E | _Rest]) when is_atom(E) ->
-%% 	[preprocess(Element) || Element <- List];
-%% preprocess(List) when is_list(List) ->
-%% 	list_to_binary(List).
-
-
-test() ->
-	Object = {networks, [
-		[{id, id}]
-	]},
-	Converted = preprocess(Object),
-	?log_debug("Converted: ~p", [Converted]),
-	Xml = xmerl:export_simple(Converted, xmerl_xml,[{prolog, ?xml_prolog}]),
-	% ?log_debug("Xml: ~p", [Xml]),
-	BinXml = unicode:characters_to_binary(Xml),
-	?log_debug("BinXml: ~p", [BinXml]).
-
-%% help to convert [atom1, atom2] to "atom1,atom2"
-convert_atom_list(AtomList) ->
-        convert_atom_list(AtomList, []).
-
-convert_atom_list([], Acc) ->
-		% ?log_debug("Acc: ~p", [Acc]),
-        Result = string:join(lists:append(Acc), ","),
-        % ?log_debug("Result: ~p", [Result]),
-        [Result];
-convert_atom_list([Atom | Tail], Acc) ->
-        convert_atom_list(Tail, [io_lib:format("~p", [Atom]) | Acc]).
+xml_preprocess(Atom) when is_atom(Atom) ->
+	[atom_to_list(Atom)];
+xml_preprocess(Integer) when is_integer(Integer) ->
+	[integer_to_list(Integer)];
+xml_preprocess(List = [{_Key, _Value} | _Rest]) ->
+	[{K, xml_preprocess(V)} || {K, V} <- List];
+xml_preprocess(ObjectList = [ [{_K,_V} | _RestObjectProps] | _RestObjectList]) ->
+	[{item, xml_preprocess(Object)} || Object <- ObjectList];
+xml_preprocess(List = [String | _Rest]) when is_list(String) ->
+	[{item, [Item]} || Item <- List];
+xml_preprocess({K, V}) ->
+	[{K, xml_preprocess(V)}];
+xml_preprocess(List = [Item | _Rest]) when is_atom(Item) ->
+	[{item, [atom_to_list(Atom)]} || Atom <- List];
+xml_preprocess(Value) ->
+	[Value].
 
 %% ===================================================================
 %% JSON Constructor Functions
