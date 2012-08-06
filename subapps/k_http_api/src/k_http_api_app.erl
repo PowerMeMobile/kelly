c13e607ae6a1676876a7d72a3f04ebc291849e54
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2012-08-06 16:21:43 +0300

    Cleanup

--- subapps/k_http_api/src/k_http_api_app.erl
+++ subapps/k_http_api/src/k_http_api_app.erl
@@ -24,15 +24,16 @@ start(_StartType, _StartArgs) ->
             {[<<"customers">>, '...'], gen_cowboy_crud, [k_http_api_handler_customers]},
             {[<<"networks">>, '...'], gen_cowboy_crud, [k_http_api_handler_networks]},
             {[<<"providers">>, '...'], gen_cowboy_crud, [k_http_api_handler_providers]},
+		    {[<<"addr2cust">>, '...'], gen_cowboy_crud, [k_http_api_handler_addr2cust]},
 			%% STATISTIC API
             {[<<"message_status">>, '_', <<"customer">>, '...'], gen_cowboy_crud, [k_http_api_handler_message_status]},
 			{[<<"report">>, <<"uplink">>], gen_cowboy_crud, [k_http_api_handler_uplink_stats]},
 			{[<<"report">>, <<"downlink">>], gen_cowboy_crud, [k_http_api_handler_downlink_stats]},
 			{[<<"report">>, <<"statuses">>], gen_cowboy_restful, [k_http_api_handler_statuses_stats]},
             {[<<"report">>, <<"messages">>, '_'], gen_cowboy_restful, [k_http_api_handler_msg_stats]},
-		    {[<<"addr2cust">>, '...'], gen_cowboy_crud, [k_http_api_handler_addr2cust]},
+			%% GUI
             {[<<"gui">>, '...'], gen_cowboy_restful, [k_http_api_handler_gui]},
-
+			%% Others handler
     		{'...', error_request_handler, []}
     	]}
 	],
