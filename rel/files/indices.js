3163d55e16ff38c11be64de6e66241c2cf0afff9
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2012-12-12 12:08:42 +0300

    Update indices and storage. Don't write gateway_id in request. It's unnecessary

--- rel/files/indices.js
+++ rel/files/indices.js
@@ -1,2 +1,3 @@
-db.outgoing_messages.ensureIndex({customer_id:1, client_type:1, in_msg_id:1}, {unique:true})
-db.outgoing_messages.ensureIndex({gateway_id:1, out_msg_id:1}, {unique:true})
+db.outgoing_messages.ensureIndex({customer_id: 1, client_type: 1, in_msg_id: 1}, {unique: true});
+db.outgoing_messages.ensureIndex({gateway_id: 1, out_msg_id: 1});
+db.outgoing_messages.ensureIndex({resp_time: 1});
