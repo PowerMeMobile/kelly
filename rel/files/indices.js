929323589a22a672b5226d9161b48876917f93f6
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2012-12-12 16:48:35 +0300

    Update indices

--- rel/files/indices.js
+++ rel/files/indices.js
@@ -1,3 +1,4 @@
 db.outgoing_messages.ensureIndex({customer_id: 1, client_type: 1, in_msg_id: 1}, {unique: true});
 db.outgoing_messages.ensureIndex({gateway_id: 1, out_msg_id: 1});
-db.outgoing_messages.ensureIndex({resp_time: 1});
+db.outgoing_messages.ensureIndex({req_time: 1});
+db.incoming_messages.ensureIndex({req_time: 1});
