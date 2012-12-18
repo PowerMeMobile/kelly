2b242dd2021f35af220ef1ba0dbb28c45a75af71
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2012-12-18 16:36:01 +0300

    Update indexes info

--- rel/files/indexes.js
+++ rel/files/indexes.js
@@ -1,9 +1,10 @@
-// create kellydb indices
+// create kellydb indexes
 db = db.getSiblingDB("kellydb");
 db.mt_messages.ensureIndex({customer_id: 1, client_type: 1, in_msg_id: 1}, {unique: true});
 db.mt_messages.ensureIndex({gateway_id: 1, out_msg_id: 1});
 db.mt_messages.ensureIndex({req_time: 1});
 db.mo_messages.ensureIndex({req_time: 1});
-// create billydb indices
+// create billydb indexes
 db = db.getSiblingDB("billydb");
+db.transactions.ensureIndex({session_id: 1, transaction_idx: 1}, {unique: true});
 db.transactions.ensureIndex({commit_time: 1});
