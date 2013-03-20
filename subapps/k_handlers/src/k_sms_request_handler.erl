087fafa739f68f6d2d704ce3ad2cd15a3cfd551a
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2013-03-20 15:51:13 +0300

    Disabled user_id in k1api req until futher investigation

--- subapps/k_handlers/src/k_sms_request_handler.erl
+++ subapps/k_handlers/src/k_sms_request_handler.erl
@@ -93,7 +93,7 @@ process_k1api_req(#just_sms_request_dto{
 	client_type = k1api,
 	id = RequestId,
 	customer_id = CustomerId,
-	user_id = UserId,
+	user_id = _UserId,
 	params = Params,
 	source_addr = SourceAddr
 }, SmsIds) ->
@@ -102,9 +102,9 @@ process_k1api_req(#just_sms_request_dto{
 	?log_debug("NotifyURL: ~p", [NotifyURL]),
 	CallbackData = get_param_by_name(<<"k1api_callback_data">>, Params),
 	?log_debug("CallbackData: ~p", [CallbackData]),
-	SubId = create_k1api_receipt_subscription(CustomerId, UserId, SourceAddr, NotifyURL, CallbackData),
+	SubId = create_k1api_receipt_subscription(CustomerId, <<"undefined">>, SourceAddr, NotifyURL, CallbackData),
 	ok = link_input_id_to_sub_id(InputMessageIds, SubId),
-	ok = link_sms_request_id_to_message_ids(CustomerId, UserId, SourceAddr, RequestId, InputMessageIds);
+	ok = link_sms_request_id_to_message_ids(CustomerId, undefined, SourceAddr, RequestId, InputMessageIds);
 process_k1api_req(_, _) ->
 	ok.
 
