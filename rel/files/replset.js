659c9727dca87ba0e34c8ee7637f8d7d699f4999
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2013-04-16 15:48:18 +0300

    Refine replica settings

--- rel/files/replset.js
+++ rel/files/replset.js
@@ -1,20 +1,13 @@
 conf = {
-        "_id" : "kelly",
-        "members" : [
-                {
-                        "_id" : 0,
-                        "host" : "10.10.0.222:27017"
-                },
-                {
-                        "_id" : 1,
-                        "host" : "10.10.0.223:27017"
-                },
-                {
-                        "_id" : 2,
-                        "host" : "10.10.0.155:27017",
-                        "arbiterOnly" : true
-                }
-        ]
+	"_id" : "kelly",
+	"members" : [
+		{"_id" : 0, "host" : "10.10.0.222:27017"},
+		{"_id" : 1, "host" : "10.10.0.223:27017"},
+		{"_id" : 2, "host" : "10.10.0.86:27017"}
+	],
+	"settings" : {
+		"getLastErrorDefaults" : {"w" : 2, "wtimeout" : 5000}
+	}
 };
 
 rs.initiate(conf);
