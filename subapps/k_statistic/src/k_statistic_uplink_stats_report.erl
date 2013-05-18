c66dc6d45e635422dc9e6aae700800242e3f5caa
Anton Sizov <a.sizov@dev1team.net>
2013-05-18 15:57:23 +0300

    Add actual rps to uplink report

--- subapps/k_statistic/src/k_statistic_uplink_stats_report.erl
+++ subapps/k_statistic/src/k_statistic_uplink_stats_report.erl
@@ -6,6 +6,7 @@
 
 -include_lib("k_common/include/logging.hrl").
 -include_lib("k_common/include/gateway.hrl").
+-include_lib("alley_dto/include/JustAsn.hrl").
 
 %% ===================================================================
 %% API
@@ -15,8 +16,11 @@
 get_report() ->
 	case k_config:get_gateways() of
 		{ok, GtwList} ->
-			{ok, GtwPropLists} = prepare_gtws(GtwList),
-			%?log_debug("GtwPropLists: ~p", [GtwPropLists]),
+			{ok, #'ThroughputResponse'{slices = Slices}} =
+				k_statistic_uplink_stats_report_helper:get_gtws_throughput(),
+			Counters =
+				lists:flatten(lists:reverse([Slice#'Slice'.counters || Slice <- Slices])),
+			{ok, GtwPropLists} = prepare_gtws(Counters, GtwList),
 			Report = {gateways, GtwPropLists},
 			{ok, Report};
 		{error, Error} ->
@@ -27,23 +31,33 @@ get_report() ->
 %% Internal
 %% ===================================================================
 
-prepare_gtws(GtwList) when is_list(GtwList) ->
-	prepare_gtws(GtwList, []);
-prepare_gtws(Gtw = {_UUID, #gateway{}}) ->
-	prepare_gtws([Gtw], []).
+prepare_gtws(Counters, GtwList) when is_list(GtwList) ->
+	prepare_gtws(Counters, GtwList, []);
+prepare_gtws(Counters, Gtw = {_UUID, #gateway{}}) ->
+	prepare_gtws(Counters, [Gtw], []).
 
-prepare_gtws([], Acc) ->
+prepare_gtws(_Counters, [], Acc) ->
 	{ok, Acc};
-prepare_gtws([{GtwUUIDBin, #gateway{}} | Rest], Acc) ->
+prepare_gtws(Counters, [{GtwUUIDBin, #gateway{}} | Rest], Acc) ->
 	GtwUUID = binary_to_list(GtwUUIDBin),
 	{ok, Name} = k_snmp:get_column_val(gtwName, GtwUUID),
 	{ok, Status} = k_snmp:get_column_val(gtwStatus, GtwUUID),
 	{ok, MaxRPS} = k_snmp:get_column_val(gtwRPS, GtwUUID),
+	{ok, ActualRpsIn} = get_actual_rps_sms(smsIn, GtwUUID, Counters),
+	{ok, ActualRpsOut} = get_actual_rps_sms(smsOut, GtwUUID, Counters),
 	GtwPropList = [
 		{id, GtwUUIDBin},
 		{name, list_to_binary(Name)},
 		{status, Status},
 		{max_rps, MaxRPS},
-		{actual_rps, 'N/A'}
+		{actual_rps_in, ActualRpsIn},
+		{actual_rps_out, ActualRpsOut}
 	],
-	prepare_gtws(Rest, [GtwPropList | Acc]).
+	prepare_gtws(Counters, Rest, [GtwPropList | Acc]).
+
+get_actual_rps_sms(_Type, _UUID, []) ->
+	{ok, 0};
+get_actual_rps_sms(Type, UUID, [#'Counter'{gatewayId = UUID, type = Type, count = Count} | _]) ->
+	{ok, Count};
+get_actual_rps_sms(Type, UUID, [_| Rest]) ->
+	get_actual_rps_sms(Type, UUID, Rest).
---
+++ subapps/k_statistic/src/k_statistic_uplink_stats_report_helper.erl
@@ -0,0 +1,47 @@
+-module(k_statistic_uplink_stats_report_helper).
+
+-export([
+	get_gtws_throughput/0
+]).
+
+-include_lib("k_common/include/logging.hrl").
+-include_lib("amqp_client/include/amqp_client.hrl").
+-include_lib("alley_dto/include/JustAsn.hrl").
+
+-define(CONTROL_KEY, <<"pmm.just.control">>).
+
+-spec get_gtws_throughput() -> ok.
+get_gtws_throughput() ->
+	{ok, Conn} = rmql:connection_start(),
+	link(Conn),
+	{ok, Chan} = rmql:channel_open(Conn),
+    {ok, Payload} = 'JustAsn':encode('ThroughputRequest', #'ThroughputRequest'{}),
+	{ok, TmpQueue} = queue_declare(Chan),
+	{ok, _ConsumerTag} = rmql:basic_consume(Chan, TmpQueue, true),
+	BProps = [{reply_to, TmpQueue}, {message_id, <<"1">>}, {content_type, <<"ThroughputRequest">>}],
+	ok = rmql:basic_publish(Chan, ?CONTROL_KEY, list_to_binary(Payload), BProps),
+	Response =
+	receive
+		{#'basic.deliver'{}, #amqp_msg{payload = RespPayload, props = RespProps}} ->
+			process_response(RespPayload, RespProps)
+	after
+		10000 ->
+			{error, timeout}
+	end,
+	unlink(Conn),
+	rmql:connection_close(Conn),
+	Response.
+
+process_response(Payload, #'P_basic'{content_type = <<"ThroughputResponse">>}) ->
+	'JustAsn':decode('ThroughputResponse', binary_to_list(Payload)).
+
+queue_declare(Chan) ->
+    Method = #'queue.declare'{durable = false,
+                              exclusive = true,
+                              auto_delete = true},
+    try amqp_channel:call(Chan, Method) of
+        #'queue.declare_ok'{queue = Queue} -> {ok, Queue};
+        Other -> {error, Other}
+    catch
+        _:Reason -> {error, Reason}
+    end.
