c9a5cfc0300b275813bd0bf0ab1aebb969a56dcd
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2014-02-28 17:52:01 +0300

    Update settings

--- rel/files/replset.js
+++ rel/files/replset.js
@@ -1,10 +1,12 @@
+// !!! Do not use 127.0.0.1 for remote replicaset
+// !!! The remote app will fail to connect to the replicaset
 conf = {
-	"_id" : "alley",
-	"members" : [
-		{"_id" : 0, "host" : "127.0.0.1:40001"},
-		{"_id" : 1, "host" : "127.0.0.1:40002"},
-		{"_id" : 2, "host" : "127.0.0.1:40003", "arbiterOnly" : true}
-	]
+    "_id" : "alley",
+    "members" : [
+        {"_id" : 0, "host" : "127.0.0.1:40001"},
+        {"_id" : 1, "host" : "127.0.0.1:40002"},
+        {"_id" : 2, "host" : "127.0.0.1:40003", "arbiterOnly" : true}
+    ]
 };
 
 rs.initiate(conf);
