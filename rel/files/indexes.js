37d3e545d0aef2ecec654e7146376e69f944c73e
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-04-18 15:47:21 +0300

    Remove old file

--- rel/files/indexes.js
+++
@@ -1,6 +0,0 @@
-// create kelly's indexes
-// moved index creating to k_storage_manager.
-// create billy's indexes
-db = db.getSiblingDB("billy");
-db.transactions.ensureIndex({si: 1, ti: 1}, {unique: true});
-db.transactions.ensureIndex({cmt: 1});
