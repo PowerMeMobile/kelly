8e9aba72b49c2b754feb65288b4459910fe371fa
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-01-09 13:44:42 +0300

    Cleanup

--- subapps/k_storage/include/storages.hrl
+++ subapps/k_storage/include/storages.hrl
@@ -1,7 +1,7 @@
 -ifndef(storages_hrl).
 -define(storages_hrl, included).
 
--type error()   :: term().
+-type error() :: term().
 
 -include_lib("alley_dto/include/addr.hrl").
 -include_lib("alley_dto/include/common_types.hrl").
