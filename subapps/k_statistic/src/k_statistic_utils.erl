f105a7ee43b97e164ef601477a9fa24a5256a298
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-02-25 17:50:32 +0300

    Remove unused functions

--- subapps/k_statistic/src/k_statistic_utils.erl
+++ subapps/k_statistic/src/k_statistic_utils.erl
@@ -1,27 +1,7 @@
 -module(k_statistic_utils).
 
 -export([
-    stats_report_frequency/0,
-
-    %% slice paths
-    msg_stats_slice_path/1,
-    msg_stats_slice_path/2,
-    status_stats_slice_path/1,
-    incoming_msg_stats_slice_path/1,
-
-    %% read/write term
-    write_term_to_file/2,
-    read_term_from_file/1,
-    read_terms_from_files/1,
-    read_terms_from_files_with/2,
-
-    %% statistic utils
-    get_timestamp_list/2,
-    get_timestamp_list/3,
-    get_timestamp_ranges/3,
-    align_time_range/2,
-    align_time_range/3,
-    get_file_list_with/3
+    get_timestamp_ranges/3
 ]).
 
 -include("application.hrl").
@@ -29,97 +9,28 @@
 -type unixepoch() :: pos_integer().
 
 %% ===================================================================
-%% Slice frequency
+%% API
+%% ===================================================================
+
+-spec get_timestamp_ranges(
+    From::unixepoch(), To::unixepoch(), Step::pos_integer()
+) -> [{unixepoch(), unixepoch()}].
+get_timestamp_ranges(From, To, Step) when From < To ->
+    Timestamps = get_timestamp_list(From, To, Step),
+    ac_lists:make_ranges(Timestamps).
+
+%% ===================================================================
+%% Internal
 %% ===================================================================
 
 -spec stats_report_frequency() -> integer().
 stats_report_frequency() ->
     60.
 
-%% ===================================================================
-%% Slice Paths
-%% ===================================================================
-
--spec slice_path(string(), [any()]) -> file:filename().
-slice_path(Fmt, Args) ->
-    {ok, CWD} = file:get_cwd(),
-    Path = lists:flatten(
-        io_lib:format(
-            "data/time-slices.d/" ++ Fmt, Args)),
-    filename:join(CWD, Path).
-
--spec msg_stats_slice_path(unixepoch()) -> file:filename().
-msg_stats_slice_path(Timestamp) ->
-    slice_path("msg-stats/~p.dat", [Timestamp]).
-
-report_type_to_index(customers) -> 1;
-report_type_to_index(networks) -> 2.
-
--spec msg_stats_slice_path(unixepoch(), atom()) -> file:filename().
-msg_stats_slice_path(Timestamp, ReportType) ->
-    slice_path("msg-stats/~p-~p.dat", [Timestamp, report_type_to_index(ReportType)]).
-
--spec status_stats_slice_path(unixepoch()) -> file:filename().
-status_stats_slice_path(Timestamp) ->
-    slice_path("status-stats/~p.dat", [Timestamp]).
-
--spec incoming_msg_stats_slice_path(unixepoch()) -> file:filename().
-incoming_msg_stats_slice_path(Timestamp) ->
-    slice_path("incoming-msg-stats/~p.dat", [Timestamp]).
-
-%% ===================================================================
-%% Read/Write term
-%% ===================================================================
-
--type filename() :: string().
--spec write_term_to_file(Term::term(), Filename::filename()) -> ok | {error, any()}.
-write_term_to_file(Term, Filename) ->
-    Binary = term_to_binary(Term, [compressed]),
-    file:write_file(Filename, Binary).
-
--spec read_term_from_file(Filename::filename()) -> {ok, term()} | {error, Reason::any()}.
-read_term_from_file(Filename) ->
-    case file:read_file(Filename) of
-        {ok, Binary} ->
-            {ok, binary_to_term(Binary)};
-        Error ->
-            Error
-    end.
-
--spec read_terms_from_files(Filenames::[file:filename()]) -> [any()].
-read_terms_from_files(Filenames) ->
-    read_terms_from_files_with(Filenames, fun(Id) -> Id end).
-
--spec read_terms_from_files_with(
-    Filenames::[file:filename()],
-    MapTermFun::fun((A::term()) -> B::term())
-) -> [any()].
-read_terms_from_files_with(Filenames, MapTermFun) ->
-    lists:foldr(
-        fun(Filename, Acc) ->
-            case k_statistic_utils:read_term_from_file(Filename) of
-                {ok, []} ->
-                    Acc;
-                {ok, Records} ->
-                    lists:map(MapTermFun, Records) ++ Acc;
-                {error, _Reason} ->
-                    %?log_debug("Missing file: ~p", [Filename])
-                    Acc
-            end
-        end,
-        [],
-        Filenames).
-
-%% ===================================================================
-%% Statistic utils
-%% ===================================================================
-
--spec get_timestamp_list(From::unixepoch(), To::unixepoch()) -> [unixepoch()].
-get_timestamp_list(From, To) when From < To ->
-    Step = stats_report_frequency(),
-    get_timestamp_list(From, To, Step).
-
--spec get_timestamp_list(From::unixepoch(), To::unixepoch(), Step::pos_integer()) -> [unixepoch()].
+-spec get_timestamp_list(
+    From::unixepoch(), To::unixepoch(), Step::pos_integer()
+) ->
+    [unixepoch()].
 get_timestamp_list(From, To, Step) when From < To ->
     {FromFloor, ToCeiling} = align_time_range(From, To),
     List = lists:seq(FromFloor, ToCeiling, Step),
@@ -128,18 +39,15 @@ get_timestamp_list(From, To, Step) when From < To ->
         false -> List
     end.
 
--spec get_timestamp_ranges(From::unixepoch(), To::unixepoch(), Step::pos_integer()) -> [{unixepoch(), unixepoch()}].
-get_timestamp_ranges(From, To, Step) when From < To ->
-    Timestamps = get_timestamp_list(From, To, Step),
-    ac_lists:make_ranges(Timestamps).
-
 -spec align_time_range(From::unixepoch(), To::unixepoch()) ->
     {FromFloor::unixepoch(), ToCeiling::unixepoch()}.
 align_time_range(From, To) ->
     Step = stats_report_frequency(),
     align_time_range(From, To, Step).
 
--spec align_time_range(From::unixepoch(), To::unixepoch(), Step::pos_integer()) ->
+-spec align_time_range(
+    From::unixepoch(), To::unixepoch(), Step::pos_integer()
+) ->
     {FromFloor::unixepoch(), ToCeiling::unixepoch()}.
 align_time_range(From, To, Step) ->
     FromFloor = From - From rem Step,
@@ -148,12 +56,3 @@ align_time_range(From, To, Step) ->
                     Rem -> To - Rem + Step
                 end,
     {FromFloor, ToCeiling}.
-
--spec get_file_list_with(
-    From::unixepoch(),
-    To::unixepoch(),
-    Fun::fun((Timestamp::unixepoch()) -> file:filename())
-) -> [file:filename()].
-get_file_list_with(From, To, Fun) when From < To ->
-    Timestamps = get_timestamp_list(From, To),
-    lists:map(Fun, Timestamps).
