13fdc75e10d581342b609465e66bd62923feb971
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-02-25 16:49:31 +0300

    Refine skip and limit in shifted storage

--- subapps/k_storage/src/shifted_storage.erl
+++ subapps/k_storage/src/shifted_storage.erl
@@ -3,11 +3,9 @@
 -export([
     find_one/2,
     find_one/3,
-    find_one/4,
 
     find/2,
     find/3,
-    find/4,
     find/5,
 
     command/1
@@ -37,13 +35,8 @@ find_one(Coll, Selector) ->
 -spec find_one(collection(), selector(), projector()) ->
     {ok, document()} | {error, reason()}.
 find_one(Coll, Selector, Projector) ->
-    find_one(Coll, Selector, Projector, 0).
-
--spec find_one(collection(), selector(), projector(), skip()) ->
-    {ok, document()} | {error, reason()}.
-find_one(Coll, Selector, Projector, Skip) ->
     {ok, Shifts} = gen_storage_manager:get_shifts(),
-    find_one(Shifts, Coll, Selector, Projector, Skip).
+    find_one(Shifts, Coll, Selector, Projector).
 
 -spec find(collection(), selector()) ->
     {ok, [{key(), document()}]} | {error, reason()}.
@@ -53,18 +46,14 @@ find(Coll, Selector) ->
 -spec find(collection(), selector(), projector()) ->
     {ok, [{key(), document()}]} | {error, reason()}.
 find(Coll, Selector, Projector) ->
-    find(Coll, Selector, Projector, 0).
-
--spec find(collection(), selector(), projector(), skip()) ->
-    {ok, [{key(), document()}]} | {error, reason()}.
-find(Coll, Selector, Projector, Skip) ->
-    find(Coll, Selector, Projector, Skip, undefined).
+    {ok, Shifts} = gen_storage_manager:get_shifts(),
+    find_no_skip_limit(Shifts, Coll, Selector, Projector, []).
 
 -spec find(collection(), selector(), projector(), skip(), limit()) ->
     {ok, [{key(), document()}]} | {error, reason()}.
 find(Coll, Selector, Projector, Skip, Limit) ->
     {ok, Shifts} = gen_storage_manager:get_shifts(),
-    find(Shifts, Coll, Selector, Projector, Skip, Limit, []).
+    find(lists:reverse(Shifts), Coll, Selector, Projector, Skip, Limit, []).
 
 -spec command(command()) ->
     {ok, document()} | {error, reason()}.
@@ -76,16 +65,14 @@ command(Command) ->
 %% Internal
 %% ===================================================================
 
-find_one([], _Coll, _Selector, _Projector, _Skip) ->
+find_one([], _Coll, _Selector, _Projector) ->
     {error, no_entry};
-find_one([ShiftDbName|Shifts], Coll, Selector, Projector, Skip) ->
+find_one([Shift | Shifts], Coll, Selector, Projector) ->
     {ok, Props} = application:get_env(?APP, shifted_storage),
-    MongoDbProps = [{mongodb_dbname, ShiftDbName} | Props],
+    Props2 = [{mongodb_dbname, Shift} | Props],
 
-    {ok, Pid} = mongodb_storage:start_link(MongoDbProps),
-
-    Res = mongodb_storage:find_one(Pid, Coll, Selector, Projector, Skip),
-
+    {ok, Pid} = mongodb_storage:start_link(Props2),
+    Res = mongodb_storage:find_one(Pid, Coll, Selector, Projector),
     ok = mongodb_storage:stop(Pid),
 
     case Res of
@@ -95,38 +82,99 @@ find_one([ShiftDbName|Shifts], Coll, Selector, Projector, Skip) ->
             find_one(Shifts, Coll, Selector, Projector)
     end.
 
-find([], _Coll, _Selector, _Projector, _Skip, _Limit, []) ->
-    {ok, []};
-find([], _Coll, _Selector, _Projector, _Skip, _Limit, Acc) ->
+find_no_skip_limit([], _Coll, _Selector, _Projector, Acc) ->
     {ok, Acc};
-find([ShiftDbName|Shifts], Coll, Selector, Projector, Skip, Limit, Acc) ->
+find_no_skip_limit([Shift | Shifts], Coll, Selector, Projector, Acc) ->
     {ok, Props} = application:get_env(?APP, shifted_storage),
-    MongoDbProps = [{mongodb_dbname, ShiftDbName} | Props],
+    Props2 = [{mongodb_dbname, Shift} | Props],
 
-    {ok, Pid} = mongodb_storage:start_link(MongoDbProps),
-
-    Res = mongodb_storage:find(Pid, Coll, Selector, Projector, Skip, Limit),
-
+    {ok, Pid} = mongodb_storage:start_link(Props2),
+    Res = mongodb_storage:find(Pid, Coll, Selector, Projector),
     ok = mongodb_storage:stop(Pid),
 
     case Res of
+        {ok, []} ->
+            find_no_skip_limit(Shifts, Coll, Selector, Projector, Acc);
         {ok, Docs} ->
-            NewAcc = Docs ++ Acc,
-            find(Shifts, Coll, Selector, Projector, Skip, Limit, NewAcc);
-        {error, no_entry} ->
-            find(Shifts, Coll, Selector, Projector, Skip, Limit, Acc)
+            Acc2 = Docs ++ Acc,
+            find_no_skip_limit(Shifts, Coll, Selector, Projector, Acc2)
+    end.
+
+find([], _Coll, _Selector, _Projector, _Skip, _Limit, Acc) ->
+    {ok, Acc};
+find([Shift | Shifts], Coll, Selector, Projector, 0, Limit, Acc) ->
+    {ok, Props} = application:get_env(?APP, shifted_storage),
+    Props2 = [{mongodb_dbname, Shift} | Props],
+
+    {ok, Pid} = mongodb_storage:start_link(Props2),
+    Res = mongodb_storage:find(Pid, Coll, Selector, Projector, 0, Limit),
+    ok = mongodb_storage:stop(Pid),
+
+    case Res of
+        {ok, []} ->
+            find(Shifts, Coll, Selector, Projector, 0, Limit, Acc);
+        {ok, Docs} ->
+            Acc2 = Acc ++ Docs,
+            case Limit - length(Docs) of
+                0 ->
+                    {ok, Acc2};
+                Rest when Rest > 0 ->
+                    find(Shifts, Coll, Selector, Projector, 0, Rest, Acc2)
+            end
+    end;
+find([Shift | Shifts], Coll, Selector, Projector, Skip, Limit, Acc) ->
+    {ok, Props} = application:get_env(?APP, shifted_storage),
+    Props2 = [{mongodb_dbname, Shift} | Props],
+
+    {ok, Pid} = mongodb_storage:start_link(Props2),
+    Command = {
+        'count', atom_to_binary(Coll, latin1),
+        Selector %% should be in {'$query, Query, ...} form
+    },
+    Res =
+        case mongodb_storage:command(Pid, Command) of
+            %% collection is missing
+            {ok, {missing, true, n, 0.0, ok, 1.0}} ->
+                {ok, [], Skip};
+            {ok, {n, 0.0, ok, 1.0}} ->
+                {ok, [], Skip};
+            {ok, {n, Count, ok, 1.0}} ->
+                Count2 = round(Count),
+                case Count2 =< Skip of
+                    true ->
+                        {ok, [], Skip - Count2};
+                    false ->
+                        case mongodb_storage:find(Pid, Coll, Selector, Projector, Skip, Limit) of
+                            {ok, Docs} ->
+                                {ok, Docs, 0};
+                            Error ->
+                                Error
+                        end
+                end
+        end,
+    ok = mongodb_storage:stop(Pid),
+
+    case Res of
+        {ok, Docs2, Skip2} ->
+            Acc2 = Acc ++ Docs2,
+            case Limit - length(Docs2) of
+                0 ->
+                    {ok, Acc2};
+                Rest when Rest > 0 ->
+                    find(Shifts, Coll, Selector, Projector, Skip2, Rest, Acc2)
+            end;
+        Error2 ->
+            Error2
     end.
 
 command([], _Command, Acc) ->
     {ok, Acc};
-command([ShiftDbName|Shifts], Command, Acc) ->
+command([Shift | Shifts], Command, Acc) ->
     {ok, Props} = application:get_env(?APP, shifted_storage),
-    MongoDbProps = [{mongodb_dbname, ShiftDbName} | Props],
+    Props2 = [{mongodb_dbname, Shift} | Props],
 
-    {ok, Pid} = mongodb_storage:start_link(MongoDbProps),
-
+    {ok, Pid} = mongodb_storage:start_link(Props2),
     Res = mongodb_storage:command(Pid, Command),
-
     ok = mongodb_storage:stop(Pid),
 
     case Res of
