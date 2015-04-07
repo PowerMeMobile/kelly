c88d3a911e3eccdaace0981cb6b3efa3bc03d000
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-04-07 11:50:30 +0300

    Get rid of snmp task queue

--- subapps/k_j3_support/src/k_j3_support_sup.erl
+++ subapps/k_j3_support/src/k_j3_support_sup.erl
@@ -21,8 +21,4 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    ?log_debug("k_j3_support_sup: init", []),
-    {ok, {{rest_for_one, 5, 10}, [
-        {k_snmp_tasks_queue, {k_snmp_tasks_queue, start_link, []}, permanent, 1000000, worker, [k_snmp_tasks_queue]},
-        {k_snmp, {k_snmp, start_link, []}, permanent, 1000000, worker, [k_snmp]}
-    ]}}.
+    {ok, {{one_for_one, 5, 10}, []}}.
--- subapps/k_j3_support/src/k_snmp.erl
+++ subapps/k_j3_support/src/k_snmp.erl
@@ -1,11 +1,6 @@
 -module(k_snmp).
 
--behaviour(gen_fsm).
-
--include("snmp_task.hrl").
--include_lib("k_storage/include/gateway.hrl").
 -include_lib("alley_common/include/logging.hrl").
--include_lib("alley_common/include/gen_fsm_spec.hrl").
 
 -define(USER, "user").
 -define(TARGET, "just").
@@ -19,33 +14,10 @@
 
 %% API
 -export([
-    start_link/0,
     get_column_val/2,
     set_row/3,
-    set_row/1,
-    del_row/2,
-    del_row/1
+    del_row/2
 ]).
-
-%% Callbacks
--export([
-    init/1,
-
-    ready/2,
-    ready/3,
-    sleep/2,
-    sleep/3,
-
-    handle_event/3,
-    handle_sync_event/4,
-    handle_info/3,
-    terminate/3,
-    code_change/4
-]).
-
--record(state, {
-    time = 10000 :: integer()
-}).
 
 -record(varbind, {
     oid     :: list(),
@@ -54,15 +26,21 @@
     int     :: integer()
 }).
 
+-type snmp_index() :: list().
+-type snmp_column_name() :: atom().
+-type snmp_value() :: term().
+-type er_snmp_state() :: term().
+-type snmp_error_reason() :: term().
+-type snmp_value_list() :: [{snmp_column_name(), snmp_value()}].
+-type snmp_result() :: {ok, snmp_value()} |
+                        {error, noSuchObject} |
+                        {error, noSuchInstance} |
+                        {error, er_snmp_state()} |
+                        {error, snmp_error_reason()}.
+
 %% ===================================================================
 %% API
 %% ===================================================================
-
--spec start_link() -> {ok, pid()}.
-start_link() ->
-    {ok, Pid} = gen_fsm:start_link({local, ?MODULE}, ?MODULE, [], []),
-    process_queue(),
-    {ok, Pid}.
 
 -spec get_column_val(snmp_column_name(), snmp_index()) -> snmp_result().
 get_column_val(ColumnName, Index) ->
@@ -72,105 +50,6 @@ get_column_val(ColumnName, Index) ->
 
 -spec set_row(snmp_column_name(), snmp_index(), snmp_value_list()) -> ok.
 set_row(TableName, Index, ValueList) ->
-    Task = #task{function = fun ?MODULE:set_row/1, args = {TableName, Index, ValueList}},
-    process_task(Task).
-
--spec del_row(snmp_column_name(), snmp_index()) -> ok.
-del_row(TableName, Index)->
-    Task = #task{function = fun ?MODULE:del_row/1, args = {TableName, Index}},
-    process_task(Task).
-
-%% ===================================================================
-%% Behaviour callbacks
-%% ===================================================================
-
-init(_Args) ->
-    {ok, ready, #state{}}.
-
--spec sleep(process_queue, #state{}) -> {next_state, sleep, #state{}};
-           (wake_up, #state{})       -> {next_state, ready, #state{}};
-           (term(), #state{})        -> {stop, {bad_event, term()}, #state{}}.
-sleep(process_queue, State = #state{}) ->
-    {next_state, sleep, State};
-sleep(wake_up, State = #state{}) ->
-    process_queue(),
-    {next_state, ready, State};
-sleep(Event, State = #state{}) ->
-    {stop, {bad_event, Event}, State}.
-
--spec ready(process_queue, #state{}) -> {next_state, ready, #state{}} | {next_state, sleep, #state{}};
-           (term(), #state{})        -> {stop, {bad_event, term()}, #state{}}.
-ready(process_queue, State = #state{time = Time}) ->
-    case next() of
-    {ok, #task{id = Id, function = Fun, args = Args}} ->
-        case Fun(Args) of
-            ok ->
-                ack(Id),
-                process_queue(),
-                {next_state, ready, State};
-            {error, _Reason} ->
-                start_timer(Time),
-                {next_state, sleep, State}
-        end;
-    {ok, []} ->
-        {next_state, ready, State}
-    end;
-ready(Event, State = #state{}) ->
-    {stop, {bad_event, Event}, State}.
-
--spec sleep(term(), term(), term()) ->
-    {stop, {bad_sync_event, term()}, term()}.
-sleep(Event, _From, State) ->
-    {stop, {bad_sync_event, Event}, State}.
-
--spec ready(term(), term(), term()) ->
-    {stop, {bad_sync_event, term()}, term()}.
-ready(Event, _From, State) ->
-    {stop, {bad_sync_event, Event}, State}.
-
-handle_event(Event, _StateName, State = #state{}) ->
-    {stop, {bad_event, Event}, State}.
-
-handle_sync_event(Event, _From, _StateName, State = #state{}) ->
-    {stop, {bad_sync_event, Event}, State}.
-
-handle_info(_Info, StateName, State = #state{}) ->
-    {next_state, StateName, State}.
-
-terminate(_Reason, _StateName, _State = #state{}) ->
-    ok.
-
-code_change(_OldVsn, StateName, State = #state{}, _Extra) ->
-    {ok, StateName, State}.
-
-%% ===================================================================
-%% Internals
-%% ===================================================================
-
-%%% Queue functions
-
-next() ->
-    k_snmp_tasks_queue:next().
-
-save_task(Task) ->
-    k_snmp_tasks_queue:save(Task).
-
-ack(Id) ->
-    k_snmp_tasks_queue:ack(Id).
-%%%
-
-start_timer(Time) ->
-    {ok, _TRef} = timer:apply_after(Time, gen_fsm, send_event, [?MODULE, wake_up]).
-
-process_queue() ->
-    gen_fsm:send_event(?MODULE, process_queue).
-
-process_task(Task) ->
-    save_task(Task),
-    process_queue().
-
--spec set_row(tuple()) -> ok | {error, term()}.
-set_row({TableName, Index, ValueList}) ->
     case is_exist(TableName, Index) of
         exist ->
             update(Index, ValueList);
@@ -183,8 +62,8 @@ set_row({TableName, Index, ValueList}) ->
             Unexpected
     end.
 
--spec del_row(tuple()) -> ok | {error, term()}.
-del_row({TableName, Index}) ->
+-spec del_row(snmp_column_name(), snmp_index()) -> ok.
+del_row(TableName, Index)->
     case TableName of
         gtw ->
             set(Index, [{gtwStatus, ?destroy}]);
@@ -198,8 +77,12 @@ del_row({TableName, Index}) ->
             {error, noSuchTable}
     end.
 
+%% ===================================================================
+%% Internal
+%% ===================================================================
+
 recreate(TableName, Index, ValueList) ->
-    del_row({TableName, Index}),
+    del_row(TableName, Index),
     create(TableName, Index, ValueList).
 
 create(TableName, Index, ValueList) ->
