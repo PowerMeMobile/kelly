bc5b6d8c064677558abda79887d5d85ebbc5bfa2
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-04-07 11:34:23 +0300

    Hide k_snmp module one more time

--- subapps/k_j3_support/src/k_j3_support.erl
+++ subapps/k_j3_support/src/k_j3_support.erl
@@ -36,7 +36,7 @@
 -spec reconfigure() -> ok.
 reconfigure() ->
     {ok, Customers} = k_storage_customers:get_customers(),
-    [k_snmp:set_customer(
+    [set_customer(
         C#customer.customer_uuid, C#customer.rps, C#customer.priority
     ) || C <- Customers],
     {ok, Gtws} = k_storage_gateways:get_gateways(),
@@ -179,9 +179,9 @@ get_funnel_connections() ->
 
 set_gtw(Gtw) ->
     GtwId = Gtw#gateway.id,
-    k_snmp:set_gateway(GtwId, Gtw#gateway.name, Gtw#gateway.rps),
-    [k_snmp:set_connection(GtwId, Conn) || Conn <- Gtw#gateway.connections],
-    [k_snmp:set_setting(GtwId, Setting) || Setting <- Gtw#gateway.settings].
+    ok = set_gateway(GtwId, Gtw#gateway.name, Gtw#gateway.rps),
+    [ok = set_connection(GtwId, Conn) || Conn <- Gtw#gateway.connections],
+    [ok = set_setting(GtwId, Setting) || Setting <- Gtw#gateway.settings].
 
 prepare_gtws(Counters, GtwList) when is_list(GtwList) ->
     prepare_gtws(Counters, GtwList, []);
