5f3c8bf60cb0e5932225513a4303828a1785d3a5
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-10-21 12:46:36 +0300

    Refine snmp stub

--- subapps/k_control/src/k_control_snmp_stub.erl
+++ subapps/k_control/src/k_control_snmp_stub.erl
@@ -1,18 +1,25 @@
 -module(k_control_snmp_stub).
 
 -export([
-    stub/0
+    enable/0,
+    disable/0
 ]).
 
 %% ===================================================================
 %% API
 %% ===================================================================
 
--spec stub() -> ok.
-stub() ->
+-spec enable() -> ok.
+enable() ->
     ok = application:start(meck),
     ok = meck:new(k_control_snmp, [non_strict]),
     ok = meck:expect(k_control_snmp, name_to_oid, fun(_) -> {ok, []} end),
     ok = meck:expect(k_control_snmp, sync_get, fun(_,_) -> {ok, 1} end),
     ok = meck:expect(k_control_snmp, sync_set, fun(_,_) -> ok end),
     ok.
+
+-spec disable() -> ok.
+disable() ->
+    ok = meck:unload(k_control_snmp),
+    {module, _} = code:load_file(k_control_snmp),
+    ok = application:stop(meck).
